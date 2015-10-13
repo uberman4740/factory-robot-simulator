719d6fda100d0128e06cdb7c7aeeaa78a79c2663
Alexander Neitz <alexander.neitz@gmail.com>
2015-10-13 16:39:08 +0200

    Made humanoids only walk during frame advancement.

---
+++ factory-robot-unityproject/Assets/Scripts/Dynamics/SimpleHumanoidWalker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityStandardAssets.Characters.ThirdPerson;
+
+public class SimpleHumanoidWalker : MonoBehaviour {
+
+//	public ThirdPersonCharacter character { get; private set; } // the character we are controlling
+
+	public TimeStepManager timeStepManager;
+	private Animator animator;
+
+	void Start() {
+//		character = GetComponent<ThirdPersonCharacter>();
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeStepManager.state != TimeStepManager.State.Advance) {
+			return;
+		}
+//		character.Move(0.3f*Vector3.right, false, false);
+		float forwardAmount = 0.1f;
+		animator.SetFloat("Forward", forwardAmount, 0.1f, timeStepManager.deltaTime);
+//		animator.SetF
+	}
+}
